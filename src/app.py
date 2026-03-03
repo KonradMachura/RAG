1f01bd3a0e54ebb05499a3b96513a52ccfb88a75
Konrad <konrad.machura44@gmail.com>
2026-03-03 19:45:40 +0100

    Adding basic gui and refactoring util so can read pdf

---
+++ src/app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+import chromadb
+from chromadb.utils import embedding_functions
+from groq import Groq
+
+st.set_page_config(page_title="HR assistant", page_icon="😎")
+
+load_dotenv()
+
+@st.cache_resource
+def load_services():
+    groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    ef = embedding_functions.SentenceTransformerEmbeddingFunction('multi-qa-MiniLM-L6-cos-v1')
+    chroma_client = chromadb.PersistentClient(path="./data")
+    collection = chroma_client.get_collection(name='company_docs', embedding_function=ef)
+    return groq_client, collection
+
+
+groq_client, collection = load_services()
+
+st.title("HR Assistant SuperTech")
+st.markdown("Ask a question about regulations, holidays or equipment!")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if user_query := st.chat_input("Write down your question..."):
+    st.chat_message("user").markdown(user_query)
+    st.session_state.messages.append({"role": "user", "content": user_query})
+
+    with st.spinner("Searching procedure..."):
+        results = collection.query(query_texts=[user_query], n_results=3)
+        documents = results['documents'][0]
+        metadatas = results['metadatas'][0]
+
+        context: str = "\n\n---\n\n".join(documents)
+
+        sources = set([meta.get("source", "Nieznane") for meta in metadatas])
+
+        prompt = f"""
+            You are a HR assistant in SuperTech company. Your task is to answer employees' questions,
+            using only the following documents chunks. If there is no answer in documents,
+            response "I am sorry, can't find any information related to your query."
+            At the end of the answer emphasis source of the informations like
+            document name and paragraph if is mentioned in context.
+            Do it in format:"Document name: ,Paragraph(if mentioned in context): "
+            Don't fabricate informations.
+            
+            Documents chunks (context):
+            {context}
+
+            Document name:
+            {sources}
+
+            Employee query:
+            {user_query}
+        """
+
+        chat_completion = groq_client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": user_query}
+            ],
+            model="llama-3.3-70b-versatile",
+            temperature=0.1
+        )
+
+        answer = chat_completion.choices[0].message.content
+
+        final_response = f"{answer}\n\n"
+
+    st.chat_message("assistant").markdown(final_response)
+    st.session_state.messages.append({"role": "assistant", "content": final_response})
