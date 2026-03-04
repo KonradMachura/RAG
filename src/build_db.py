95af768b5161b8684e22b4269349bb644fafac76
Konrad <konrad.machura44@gmail.com>
2026-03-04 21:53:34 +0100

    Creating semantic chunking

--- src/build_db.py
+++ src/build_db.py
@@ -14,17 +14,20 @@ def main():
         , they act like a "key and lock".
     """
     # sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction('all-MiniLM-L6-v2')
-    sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction('multi-qa-MiniLM-L6-cos-v1')
+    # sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction('multi-qa-MiniLM-L6-cos-v1')
+    sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction('paraphrase-multilingual-MiniLM-L12-v2')
 
     chroma_client = chromadb.PersistentClient(path="./data")
     collection = chroma_client.get_or_create_collection(
-        name='company_docs',
+        # name='company_docs',
+        name='hobbit_book',
         embedding_function=sentence_transformer_ef)
 
     docs_contents, docs_names, docs_paths = u.read_docs()
     for i, (doc_name, doc_content) in enumerate(zip(docs_names, docs_contents)):
         # print(f"i={i}, doc_name={doc_name}, doc_content={doc_content[:10]}")
-        chunks: list[str] = c.paragraph_chunking(doc_content)
+        # chunks: list[str] = c.paragraph_chunking(doc_content)
+        chunks: list[str] = c.semantic_chunking(doc_content)
         chunks_ids: list[str] = [f"chunk_{j}_{doc_name}" for j in range(len(chunks))]
         metadatas: list[dict[str,str]] = [ {"source": doc_name} for _ in chunks]
 
